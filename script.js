4a07e3a0f682ffa6801d474e409662e095371f66
Your Name <you@example.com>
2025-02-23 21:00:23 +0530

    added active and complete toggle button functionality

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
+//UI constants
 const form = document.querySelector("#todo-form"); //get the form element
 const list = document.querySelector("#todo-list");
 const buttonAddTodo = document.querySelector("#button-add-todo");
+const toggles = document.querySelectorAll(".todo-type-toggles > button");
 
-//Get Data
+//Enums
+const states = {
+  ACTIVE: "Active",
+  COMPLETED: "Completed",
+};
+
+//Get Data on page load
 let TODOs = []; //array to store the tasks
-//make sure the local storage is not empty before parsing it
-if (localStorage["data"] !== null && localStorage["data"] !== undefined) {
-  TODOs = JSON.parse(localStorage["data"]); //parse out the data
+try {
+  if (localStorage["data"]) {
+    TODOs = JSON.parse(localStorage["data"]); //parse out the data
+  }
+  if (!Array.isArray(TODOs)) {
+    TODOs = [];
+  }
+} catch (e) {
+  console.error("Error parsing local storage data", e);
+  TODOs = [];
 }
 
-function buildUI() {
+function buildUI(state) {
   let HTML = ``;
-  TODOs.forEach((todo) => {
-    HTML += `<li id="${todo.id}" style="view-transition-name:list-item-${todo.id};">
-    ${todo.title}
+  let viewTODOs = [];
+
+  if (state === states.COMPLETED) {
+    viewTODOs = TODOs.filter((todo) => todo.complete);
+  } else {
+    viewTODOs = TODOs.filter((todo) => !todo.complete);
+  }
+
+  if (viewTODOs.length === 0) {
+    HTML = `<li class="empty">Nothing to do!</li>`;
+  }
+  viewTODOs.forEach((todo) => {
+    if (todo !== null) {
+      HTML += `<li id="${todo.id}" style="view-transition-name:list-item-${todo.id};" data-complete="${todo.complete}">
+    <span class="text">${todo.title}</span>
     <button aria-label="Complete" class="button-complete">
       <svg width="20" height="20" viewBox="0 0 241.44 259.83" class="svg-check">
         <polyline pathLength="1" points="16.17 148.63 72.17 225.63 225.17 11.63"/>
       </svg>  
     </button>
     </li>`;
+    }
   });
   console.log("HTML for todo list", HTML);
   list.innerHTML = HTML;
@@ -48,7 +76,7 @@ function addTodo() {
   TODOs.push({
     //add the task to the array
     title: form[0].value, //event.target[0] is the input field
-    completed: false, //when we add a new task, it is not completed. So, false
+    complete: false, //when we add a new task, it is not completed. So, false
     id: self.crypto.randomUUID(), //browser gives a unique id for free
   });
 
@@ -59,27 +87,100 @@ function addTodo() {
 
 document.documentElement.addEventListener("click", (event) => {
   if (event.target.classList.contains("button-complete")) {
-    removeTodo(event);
+    toggleTodo(event);
   }
 });
 
-function removeTodo(event) {
+list.addEventListener("dblclick", (event) => {
+  const listItem = event.target.closest("li");
+
+  // If already editing, let it be.
+  if (listItem.classList.contains("editing")) return;
+
+  listItem.classList.add("editing");
+  const textItem = listItem.querySelector(".text");
+  listItem.insertAdjacentHTML(
+    "beforeend",
+    ` <form onsubmit="updateTodo(event);" class="form-edit">
+      <input onblur="updateTodo(event);" type="text" class="input-edit" value="${textItem.textContent}" />
+    </form>`
+  );
+
+  const input = listItem.querySelector(".input-edit");
+  input.focus();
+
+  //put cursor at end of input
+  input.setSelectionRange(input.value.length, input.value.length);
+});
+
+function updateTodo(event) {
+  event.preventDefault();
+
+  const listItem = event.target.closest("li");
+  const textItem = listItem.querySelector(".text");
+  const inputItem = listItem.querySelector(".input-edit");
+  const form = listItem.querySelector(".form-edit");
+
+  textItem.textContent = inputItem.value;
+  listItem.classList.remove("editing");
+  form.remove();
+
+  TODOs = TODOs.map((todo) => {
+    if (todo.id === listItem.id) {
+      todo.title = inputItem.value;
+    }
+    return todo;
+  });
+  localStorage["data"] = JSON.stringify(TODOs);
+}
+
+function toggleTodo(event) {
   const listItem = event.target.parentElement;
   console.log("list item", listItem);
+  console.log("list item dataset", listItem.dataset);
   //Trigger complete animation
   listItem.classList.toggle("complete");
-  console.log("list item", listItem.classList);
   setTimeout(() => {
-    TODOs = TODOs.filter((todo) => todo.id !== event.target.parentElement.id);
-    localStorage["data"] = JSON.stringify(TODOs);
-
-    if (!document.startViewTransition) {
-      buildUI();
+    if (listItem.dataset.complete === "true") {
+      TODOs = TODOs.filter((todo) => !todo.complete);
+      if (!document.startViewTransition) {
+        buildUI(states.COMPLETED);
+      } else {
+        document.startViewTransition(() => {
+          buildUI();
+        });
+      }
     } else {
-      document.startViewTransition(() => {
-        buildUI();
+      TODOs.forEach((todo) => {
+        if (todo.id === listItem.id) {
+          todo.complete = !todo.complete;
+        }
       });
+      if (!document.startViewTransition) {
+        buildUI(states.ACTIVE);
+      } else {
+        document.startViewTransition(() => {
+          buildUI();
+        });
+      }
     }
+    localStorage["data"] = JSON.stringify(TODOs);
   }, 1000);
 }
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", (event) => {
+    toggles.forEach((toggle) => {
+      toggle.setAttribute("aria-pressed", false);
+    });
+    toggle.setAttribute("aria-pressed", true);
+
+    if (toggle.textContent === states.ACTIVE) {
+      buildUI(states.ACTIVE);
+    } else {
+      buildUI(states.COMPLETED);
+    }
+  });
+});
+
 buildUI();
